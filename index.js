962eceb849d395a9132671db33b619b6dbc17c7f
Yashaswi Anand <yash.kwr17@gmail.com>
2022-05-22 00:31:14 +0530

    Layout completed and post method app

--- index.js
+++ index.js
@@ -11,13 +11,42 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// list of todo list
+const todoList = [
+    {
+        discription: 'Learn Node.js',
+        category: 'Node.js',
+        due_date: '2020-01-01',
+    },
+    {
+        discription: 'Learn express.js',
+        category: 'Node.js',
+        due_date: '2020-21-01',
+    },
+    
+]
 
 // add get route
 app.get('/', (req, res) => {
     res.render('home',{
         title:"TODO APP",
+        todoList: todoList,
     });
 });
+// url encoded data
+app.use(express.urlencoded());
+// post method
+app.post('/todo-_list',function(req,res){
+    console.log(req.body);
+    todoList.push(req.body);
+    // todoList.push({
+    //     discription: req.body.discription,
+    //     category: req.body.category,
+    //     due_date: req.body.due_date,
+    // });
+    return res.redirect('/');
+});
+
 
 // listen to the port
 app.listen(port, function(err){
